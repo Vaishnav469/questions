72b297edc55a9a753243a995f044cf4530513214
Vaishnav Mandru <106818193+Vaishnav469@users.noreply.github.com>
2026-06-28 18:39:32 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        hashmap = {}
+        for prereq in prerequisites:
+            if prereq[0] not in hashmap:
+                hashmap[prereq[0]] = []
+            hashmap[prereq[0]].append(prereq[1])
+        processed = set()
+        def dfs(num, visit):
+            if num not in hashmap:
+                return True
+            result = True
+            for n in hashmap[num]:
+                if n in visit:
+                    return False
+                visit.add(n)
+                if dfs(n, visit):
+                    processed.add(n)
+                else:
+                    return False
+                visit.remove(n)
+
+            return True
+                
+                
+        
+            
+        for i in range(numCourses):
+            visit = set()
+            visit.add(i)
+            if not dfs(i, visit):
+                return False
+        return True
+        
+
+            
